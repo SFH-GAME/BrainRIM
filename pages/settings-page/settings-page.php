d2b27a2c87fd03ad9ff6e336c9ad1367db21ac4a
MooNMisTX <butor.aleksey@mail.ru~>
2025-04-04 15:22:49 +0300

    updated version

--- pages/settings-page/settings-page.php
+++ pages/settings-page/settings-page.php
@@ -10,7 +10,7 @@ include($_SERVER['DOCUMENT_ROOT'] . "/dataBase/controllers/settings/userQuestion
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet">
-   <link rel="stylesheet" href="/pages/settings-page/css/settings-page.css?v=2.0">
+   <link rel="stylesheet" href="/pages/settings-page/css/settings-page.css?v=1.0">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
    <link rel="stylesheet" href="/BrimFont/css/brim.css">
    <title>Настройки</title>
@@ -173,7 +173,7 @@ include($_SERVER['DOCUMENT_ROOT'] . "/dataBase/controllers/settings/userQuestion
 			<div class="hover mark"><img src="/img/icons/shield-half-outline.svg" class="mark-icon" alt="иконка-щит" title="иконка-щит"></div>
          </a>
       </footer>
-      <script src="/pages/settings-page/settings-page.js?v=2.0"></script>
+      <script src="/pages/settings-page/settings-page.js?v=1.0"></script>
 </body>
 
 </html>
